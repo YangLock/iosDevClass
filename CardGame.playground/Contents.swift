daccab9e7f0dcbc56a06fbc72b7921ccb39829a3
YangLock <3167706386@qq.com>
2019-09-03 22:33:44 +0800

    first homework

---
+++ CardGame.playground/Contents.swift
@@ -0,0 +1,46 @@
+/*:
+ ## Two functions for automatically discard and randomly choose a card
+ - **chooseOne** is a function which can randomly choose a card
+ - **autoDiscard** is a function which can automatically discard
+*/
+var cards = [(1,"spade"),(2,"spade"),(3,"spade"),(4,"spade"),(5,"spade"),(6,"spade"),(7,"spade"),(8,"spade"),(9,"spade"),(10,"spade"),(11,"spade"),(12,"spade"),(13,"spade"),
+             
+             (1,"heart"),(2,"heart"),(3,"heart"),(4,"heart"),(5,"heart"),(6,"heart"),(7,"heart"),(8,"heart"),(9,"heart"),(10,"heart"),(11,"heart"),(12,"heart"),(13,"heart"),
+             
+             (1,"club"),(2,"club"),(3,"club"),(4,"club"),(5,"club"),(6,"club"),(7,"club"),(8,"club"),(9,"club"),(10,"club"),(11,"club"),(12,"club"),(13,"club"),
+             (1,"diamond"),(2,"diamond"),(3,"diamond"),(4,"diamond"),(5,"diamond"),(6,"diamond"),(7,"diamond"),(8,"diamond"),(9,"diamond"),(10,"diamond"),(11,"diamond"),(12,"diamond"),(13,"diamond")
+]
+
+
+var num : Int = 51
+let number: Int = 51
+
+func chooseOne() -> (num : Int, color : String){
+    let i : Int = Int.random(in: 0...num)
+    if num > 0{
+        num = num - 1;
+        let t = cards[number]
+        cards[number] = cards[i]
+        cards[i] = t
+        return cards[number]
+    }else{
+        print("All the cards have been discarded!")
+        return (0,"none")
+    }
+}
+var cardsOfPlayers: [[(num: Int, color: String)]] = []
+
+func autoDiscard(numOfPlayers: Int, numOfCards: Int) -> [[(num: Int, color: String)]]{
+    for i in 0..<numOfPlayers{
+        cardsOfPlayers.append([])
+        for _ in 0..<numOfCards{
+            cardsOfPlayers[i].append(chooseOne())
+        }
+    }
+    return cardsOfPlayers
+}
+
+autoDiscard(numOfPlayers: 5, numOfCards: 4)
+for i in 0..<5 {
+    print(cardsOfPlayers[i])
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Homework1
+### 两个函数实现自动发牌和随机抽牌功能（发牌时传进两个参数：玩家个数，每个玩家的手牌个数）
+- chooseOne
+- autoDiscard
+
+#### 注：在Xcode10.3之后的版本中无法使用arc4random产生随机数，因此使用了Int.random(in: range)这个Int自带的产生随机数的函数
+
