3a24a6c16737445191cacb766e395490f5380ea4
YangLock <3167706386@qq.com>
2019-10-09 22:50:55 +0800

    remove submodule

---
+++ myProject/myProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by victor on 2019/9/24.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
